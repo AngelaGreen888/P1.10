5942fe746723a1e2e1e6b09de24ddf4457d1d53a
Jack Chen <jchen814@gmail.com>
2019-10-20 19:03:20 -0500

    added buttons

--- sketch_191018a/sketch_191018a.js
+++ sketch_191018a/sketch_191018a.js
@@ -1,35 +1,59 @@
 let capture;
 let pg;
 let plus;
+var plusButton;
+var lightButton;
 
 function setup() {
   createCanvas(1280, 720);
+  background(0,100,120);
+  
   capture = createCapture(VIDEO);
+  capture.size(1280, 720);
+  capture.hide();
+  
   pg = createGraphics(1280, 720);
+  pg.background(0, 0, 0, 0);
+  pg.noStroke();
   
   plus = loadImage('Assets/Plus.png');
   lightbulb = loadImage('Assets/Light_Bulb.png');
   healthAppLogo = loadImage('Assets/Apple_Health.png');
   youtubeAppLogo = loadImage('Assets/Youtube.png');
   
-  capture.size(1280, 720);
-  capture.hide();
-}
-
-function draw() {
-  background(0,100,120);
-  pg.background(0, 0, 0, 0);
-  pg.noStroke();
-  
   let c = color(255, 204, 0);
   pg.fill(c);
   pg.circle(30, pg.height-30, 20);
-  pg.image(plus, 15, pg.height-45, 31, 31);
+  plusButton = createImg('Assets/Plus.png');
+  plusButton.position(34, pg.height-64);
+  plusButton.size(31, 31);
+  plusButton.mousePressed(addApps);
   
   c = color(255, 204, 0);
   pg.fill(c);
   pg.circle(80, pg.height-30, 20);
-  pg.image(lightbulb, 65, pg.height-46, 31, 31);
+  lightButton = createImg('Assets/Light_Bulb.png');
+  lightButton.position(82, pg.height-64);
+  lightButton.size(31, 31);
+  lightButton.mousePressed(changeBG);
+  
+}
+
+function changeBG() {
+  val1 = random(255);
+  val2 = random(255);
+  val3 = random(255);
+  background(val1, val2, val3);
+}
+
+function addApps() {
+  pg.clear();
+  
+  pg.circle(80, 30, 20);
+}
+
+function draw() {
+  
   
   pg.image(healthAppLogo, 0, pg.height-550, 85, 85);
   
